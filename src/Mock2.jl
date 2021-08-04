95193ae66ef06f9e6247ebfd818081e5a216d86a
mcodevb <getmainul1@gmail.com>
2021-08-05 01:54:03 +0530

    Added some functions

--- src/Mock2.jl
+++ src/Mock2.jl
@@ -1,5 +1,17 @@
 module Mock2
 
-# Write your package code here.
+# add dependencies here
+using DelimitedFiles
+
+# functions to export
+export addone, addtwo
+
+function addone(x)
+    return x+1
+end
+
+function addtwo(x)
+    return x+2
+end
 
 end
