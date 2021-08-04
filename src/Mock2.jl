54bbb69e89d69ece9c04f6ae45d5a17e0bfce5b0
mcodevb <getmainul1@gmail.com>
2021-08-05 02:23:09 +0530

    Added two more functions and dependency

--- src/Mock2.jl
+++ src/Mock2.jl
@@ -1,10 +1,10 @@
 module Mock2
 
 # add dependencies here
-using DelimitedFiles
+using DelimitedFiles, ForwardDiff, TickTock
 
 # functions to export
-export addone, addtwo
+export addone, addtwo, my_f, my_f_der
 
 function addone(x)
     return x+1
@@ -13,5 +13,9 @@ end
 function addtwo(x)
     return x+2
 end
+
+my_f(x)=x^2-5x+1;
+my_f_der(x)=ForwardDiff.derivative(my_f,x)
+
 
 end
